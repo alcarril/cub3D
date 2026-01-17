e85b9c095d40bd96346f96bdc67e20e89119e148
alex <alexcarrillomc@outlook.com>
2026-01-17 15:24:01 +0100

    Refactorizon funciones de ambientes y documento

--- src/ambiances/ambiance_configs.c
+++ src/ambiances/ambiance_configs.c
@@ -6,35 +6,50 @@
 /*   By: alejandro <alejandro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 19:14:29 by alejandro         #+#    #+#             */
-/*   Updated: 2026/01/16 11:57:11 by alejandro        ###   ########.fr       */
+/*   Updated: 2026/01/16 20:29:55 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cube3D.h"
 
+/*
+	Configuracion de la ambiance cementery
+	- Fog color: Dark medium gray
+	- Max distance: Medium distance
+	- Fog multipliers: 1.7 for walls, floor, and ceiling (mucha niebla)
+	- Shader k-factors: 8.0 for walls, 1.0 for floor and ceiling
+	- Shader multipliers: 1.0 for walls, floor, and ceiling
+	- Ambiance type: CEMENTERY
+*/
 void	config_ambiance_cementery(t_ambiance *amb)
 {
 	amb->fog_color_walls = FOG_MEDIO_OSCURO;
 	amb->fog_color_fc = FOG_MEDIO_OSCURO;
 	amb->v_max_distance_map = MEDIUM_DISTANCE;
 	amb->vinv_max_diatance = 1.0f / amb->v_max_distance_map;
-	
 	amb->mult_fog_walls = 1.7f;
 	amb->mult_fog_floor = 1.7f;
 	amb->mult_fog_ceiling = 1.7f;
-	
 	amb->k_factor_floor = 1.0f;
 	amb->k_factor_ceiling = 1.0f;
 	amb->k_factor_walls = 8.0f;
-	
 	amb->mult_shader_walls = 1.0f;
 	amb->mult_shader_floor = 1.0f;
 	amb->mult_shader_ceiling = 1.0f;
 	amb->ambiance = CEMENTERY;
 }
 
-
-void	config_ambiance_asturias1(t_map *map, t_ambiance *amb)
+/*
+	Configuracion de la ambiance asturias
+	- Fog color: Light medium gray
+	- Max distance: 70% of map max distance
+	- Fog multipliers: 1.7 for walls and floor, 1.0 for ceiling
+	- Shader k-factors: 8.0 for walls, 3.0 for floor, 1.0 for ceiling
+	- Shader multipliers: 1.0 for walls, 1.3 for floor, 0.3 for ceiling
+	- Ambiance type: ASTURIAS
+	- Configuracion para ambientes abiertos	y luminosos con niebla moderada
+*/
+void	config_ambiance_asturias(t_map *map, t_ambiance *amb)
 {
 	amb->fog_color_walls = FOG_MEDIO_CLARO;
 	amb->fog_color_fc = FOG_MEDIO_CLARO;
@@ -43,41 +58,25 @@ void	config_ambiance_asturias1(t_map *map, t_ambiance *amb)
 	amb->mult_fog_walls = 1.7f;
 	amb->mult_fog_floor = 1.7f;
 	amb->mult_fog_ceiling = 1.0f;
-	
 	amb->k_factor_walls = 8.0f;
 	amb->k_factor_floor = 3.0f;
 	amb->k_factor_ceiling = 1.0f;
-	
 	amb->mult_shader_walls = 1.0f;
 	amb->mult_shader_floor = 1.3f;
 	amb->mult_shader_ceiling = 0.3f;
-	
 	amb->ambiance = ASTURIAS;
 }
 
-void	config_ambiance_asturias(t_map *map, t_ambiance *amb)//neo
-{
-	// amb->fog_color_walls = 0x437317;
-	amb->fog_color_walls = 0x71DB00;
-	amb->fog_color_fc = 0x71DB00;
-	amb->v_max_distance_map = map->max_distance * 0.7f;
-	amb->vinv_max_diatance = 1.0f / amb->v_max_distance_map;
-	amb->mult_fog_walls = 0.3f;
-	amb->mult_fog_floor = 0.4f;
-	amb->mult_fog_ceiling = 0.3f;
-	
-	amb->k_factor_walls = 8.0f;
-	amb->k_factor_floor = 3.0f;
-	amb->k_factor_ceiling = 1.0f;
-	
-	amb->mult_shader_walls = 1.0f;
-	amb->mult_shader_floor = 1.3f;
-	amb->mult_shader_ceiling = 0.3f;
-	
-	amb->ambiance = ASTURIAS;
-}
-
-
+/*
+	Configuracion de la ambiance open
+	- Fog color: Dark medium gray for walls, light medium gray for floor and ceiling
+	- Max distance: 90% of map max distance
+	- Fog multipliers: 0.2 for walls and ceiling, 0.3 for floor
+	- Shader k-factors: 8.0 for walls, 4.0 for ceiling, 1.0 for floor
+	- Shader multipliers: 1.0 for walls, 0.5 for ceiling, 0.7 for floor
+	- Ambiance type: OPEN
+	- Configuracion para ambientes abiertos	y luminosos simula exteriores
+*/
 void	config_ambiance_open(t_map *map, t_ambiance *amb)
 {
 	amb->fog_color_walls = FOG_MEDIO_OSCURO;
@@ -87,50 +86,40 @@ void	config_ambiance_open(t_map *map, t_ambiance *amb)
 	amb->mult_fog_walls = 0.2f;
 	amb->mult_fog_floor = 0.3f;
 	amb->mult_fog_ceiling = 0.2f;
-	
 	amb->k_factor_walls = 8.0f;
 	amb->k_factor_floor = 1.0f;
 	amb->k_factor_ceiling = 4.0f;
-	
 	amb->mult_shader_walls = 1.0f;
 	amb->mult_shader_floor = 0.7f;
 	amb->mult_shader_ceiling = 0.5f;
-
 	amb->ambiance = OPEN;
-
 }
 
-//hecho yes
-float	dist_factor_floor(int win_height, int win_y, int horizon, int ambient)
+
+/*
+	Configuracion de la ambiance matrix
+	- Fog color: Greenish
+	- Max distance: 70% of map max distance
+	- Fog multipliers: 0.3 for walls, 0.4 for floor, 0.3 for ceiling
+	- Shader k-factors: 8.0 for walls, 3.0 for floor, 1.0 for ceiling
+	- Shader multipliers: 1.0 for walls, 1.3 for floor, 0.3 for ceiling
+	- Ambiance type: ASTURIAS
+	- Configuracion para ambientes abiertos	y luminosos con niebla moderada
+*/
+void	config_ambiance_matrix(t_map *map, t_ambiance *amb)
 {
-	float	dist_factor;
-
-	if (ambient == CEMENTERY)
-		dist_factor = 1.0f - (float)(horizon - win_y) / (win_height - horizon);
-	else if (ambient == ASTURIAS)
-		dist_factor = (float)( win_y - horizon) / (win_height - horizon);
-	else if (ambient == OPEN)
-		dist_factor = 1.0f - (float)( win_y - horizon) / (win_height - horizon);
-	if (dist_factor < 0.0f)
-		dist_factor = 0.0f;	
-	if (dist_factor > 1.0f)
-		dist_factor = 1.0f;
-	return (dist_factor);
-}
-
-float	dist_factor_ceiling(int win_y, int horizon, int ambient)
-{
-	float	dist_factor;
-
-	if (ambient == CEMENTERY)
-		dist_factor = 1.0f - (float)(win_y - horizon) / horizon;
-	else if (ambient == ASTURIAS)
-		dist_factor = 1.0f - (float)(horizon - win_y) / horizon;
-	else if (ambient == OPEN)
-		dist_factor = (float)(horizon - win_y) / horizon;
-	if (dist_factor < 0.0f)
-		dist_factor = 0.0f;	
-	if (dist_factor > 1.0f)
-		dist_factor = 1.0f;
-	return (dist_factor);
+	amb->fog_color_walls = 0x71DB00;
+	amb->fog_color_fc = 0x71DB00;
+	amb->v_max_distance_map = map->max_distance * 0.7f;
+	amb->vinv_max_diatance = 1.0f / amb->v_max_distance_map;
+	amb->mult_fog_walls = 0.3f;
+	amb->mult_fog_floor = 0.4f;
+	amb->mult_fog_ceiling = 0.3f;
+	amb->k_factor_walls = 8.0f;
+	amb->k_factor_floor = 3.0f;
+	amb->k_factor_ceiling = 1.0f;
+	amb->mult_shader_walls = 1.0f;
+	amb->mult_shader_floor = 1.3f;
+	amb->mult_shader_ceiling = 0.3f;
+	amb->ambiance = MATRIX;
 }
--- src/ambiances/shaders.c
+++ src/ambiances/shaders.c
@@ -6,13 +6,27 @@
 /*   By: alejandro <alejandro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:01:39 by alejandro         #+#    #+#             */
-/*   Updated: 2026/01/14 16:07:39 by alejandro        ###   ########.fr       */
+/*   Updated: 2026/01/16 20:43:14 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3D.h"
 
-// Funcion base aplicasr shde a  un color
+/*
+	Funcion para aplicar las sombra a un color. Cuando descomponemos el color en sus
+	componentes RGB, multiplicamos cada componente por el factor de sombra (shade).
+	Primero se descompone el color, una vez decoompuesto se aplica el factor de sombra
+	que es una valor entre 0.0 y 1.0. Esto es poruqe los coloeres cuando se oscurecen
+	mantiene la misma porportcion entre rgb para que el color no cambien pero cuanto mayor
+	sea el valor de las componenertes mas oscuro se vera el color resultante cunado se vuelva
+	a componer. Finalmente se vuelve a componer el color y se retorna.
+	Mejoras de microporcesador:
+	- Se usan enteros para las operaciones de multiplicacion y desplazamiento de bits
+	  en lugar de flotantes, lo que es mas rapido en la mayoria de los microprocesadores.
+	- Se evitan operaciones de division y modulo, que son mas lentas.
+	- Se usan operaciones de desplazamiento de bits para extraer y combinar los componentes RGB,
+	  que son mas eficientes que las operaciones aritmeticas tradicionales. (mucho menos ciclos de procesador)
+*/
 unsigned int apply_shade(unsigned int color, float shade)
 {
 	int r = (int)(((color >> 16) & 255) * shade);
@@ -31,10 +45,26 @@ unsigned int apply_shade(unsigned int color, float shade)
 		b = 255; 
 	if (b < 0) 
 		b = 0;
-	return (r << 16) | (g << 8) | b;
+	return ((r << 16) | (g << 8) | b);
 }
 
-//Fucnion para plizar el shade lineal
+/*
+	Estas funciones sirven para poder calcular normalizar el factor de shde entre 0.0 y 1.0
+	en funcion de la distancia y la distancia maxima. El factor de sombra se calcula restando
+	la proporcion de la distancia actual sobre la distancia maxima a 1.0. Si la distancia es
+	mayor que la distancia maxima, el factor de sombra se establece en 0.0 (completamente oscuro).
+	Con esto conseguimos simular profundidad y atenuacion de la luz con la distancia sin tener que 
+	usar un postporcesado o precporcesado de la imgagen, lo hacemos mientras elegimos el color de
+	cada pixel de la matriz de pixeles de la pantalla cunado estamos renderizando las paredes, suelo y techo.
+	Cada una de estas fucniones usa un metodo diferente para calcular el factor de sombra.
+	- Metodo lineal: El factor de sombra disminuye linealmente con la distancia. Es el metodo
+	  mas sencillo y comunmente usado. pero no simula bien nieblas densas o efectos atmosfericos.
+	- Metodo inverso: El factor de sombra disminuye inversamente con la distancia. Ete metodo es
+	  mas eficiente computacionalmente y simula nieblas ligeras y efectos atmosfericos.
+	- Metodo exponencial: El factor de sombra disminuye exponencialmente con la distancia Este es muy caro
+	  computacionalmente hablando pero simula nieblas densas y efectos atmosfericos.
+*/
+
 unsigned int shade_linear(unsigned int color, float dist, float max_dist)
 {
 	float shade = 1.0f - (dist / max_dist);
@@ -42,29 +72,22 @@ unsigned int shade_linear(unsigned int color, float dist, float max_dist)
 		shade = 0.0f;
 	if (shade > 1.0f) 
 		shade = 1.0f;
-
-	return apply_shade(color, shade);
+	return (apply_shade(color, shade));
 }
 
-//metodo inverso de shadin (recomendado) -> k >1 y proportion dis (0.1)
-//optimizacion multiplicamos por porportion dist el k que alfinal es lo miso que 
-//hacer el k proporciol y multiplicarlo por dis y ademas asi n o se hace la opracion en cadapixell
-///de la pantalla menos ciclos de procesador
 unsigned int shade_inverse(unsigned int color, float k, float proportion_dist)
 {
 	float	shade;
 	
 	shade  = 1.0f / (1.0f + proportion_dist * k);
-	return apply_shade(color, shade);
+	return (apply_shade(color, shade));
 }
 
-//medodo exponencial niebla densa density > 1.0	proportion dist (0.1)
-//meto optimizacion de proportion dist
 unsigned int shade_exponential(unsigned int color, float density, float proportion_dist)
 {	
 	float shade;
 	
 	shade  = expf(-proportion_dist * density);
-	return apply_shade(color, shade);
+	return (apply_shade(color, shade));
 }
 
